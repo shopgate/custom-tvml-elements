2b5d3ae9f129c7a73a4033074ec805f353653e5a
Alex Goergisn <goergisn@me.com>
2016-01-25 14:31:11 +0100

    Removing Folder to add Files to Git

---
+++ app/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SampleApp
+//
+//  Created by Alex Goergisn on 19.12.15.
+//  Copyright © 2015 Goergisn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TVMLKit/TVMLKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) TVApplicationController *appController;
+
+
+@end
+
---
+++ app/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Alex Goergisn on 19.12.15.
+//  Copyright © 2015 Goergisn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
